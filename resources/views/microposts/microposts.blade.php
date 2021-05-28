41321b277b799f35de3b8bd569b0bd39d7773f79
chinatsu.haraikawa <pechi72@icloud.com>
2021-05-28 06:22:06 +0000

    favorites

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -14,6 +14,11 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
+                    
+                    <div class="mr-2">
+                        {{-- お気に入り追加／削除ボタン --}}
+                        @include('favorite.favorite_button')
+                    </div>
                     <div>
                         @if (Auth::id() == $micropost->user_id)
                             {{-- 投稿削除ボタンのフォーム --}}
